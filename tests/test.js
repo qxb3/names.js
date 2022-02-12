17e2b7111683a683d8cf97af3f1e3c2bd33f9c39
qxb3 <toolong972@gmail.com>
2022-02-12 15:48:11 +0800

    Removed tests

--- tests/test.js
+++
@@ -1,5 +0,0 @@
-const { generateHumanName, generateAnimalName, generatePokemonName } = require('../')
-
-console.log('HUMANS', generateHumanName('female', 0))
-console.log('ANIMALS', generateAnimalName(100))
-console.log('POKEMONS', generatePokemonName(0))
